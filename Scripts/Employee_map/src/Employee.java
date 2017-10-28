3a779499e6389a983e72cb9a8b17d82f3ab41a5a
yaposebastien <yaposebastien@gmail.com>
2017-10-28 05:35:27 -0400

    Adding some comments to class

--- Scripts/Employee_map/src/Employee.java
+++ Scripts/Employee_map/src/Employee.java
@@ -1,11 +1,17 @@
-
+/*
+ * An Employee class with empNumber, empName, and empSalary
+ */
 public class Employee {
 
 	private byte empNumber;
 	private String empName;
 	private double empSalary;
 	
-	//Construct Employee default constructor
+	/**
+	 * Constructs an employee with 0.0 as salary
+	 * @param number : the number of the employee
+	 * @throws Exception according to employee number greater than 0 and less 50
+	 */
 	public Employee(byte number) throws Exception {
 		
 		if(number >= 0 && number <= 50)
@@ -16,12 +22,31 @@ public class Employee {
 		empSalary = 0.0;	
 	}
 	
-	//Construct Employee constructor
+	/**
+	 * Construct an Employee
+	 * @param number : the number of the employee
+	 * @param name : the name of the employee
+	 * @param salary : the salary of the employee
+	 * @throws Exception
+	 */
+	
+	/**
+	 * Default constructor
+	 * @param number
+	 * @param name
+	 * @param salary
+	 * @throws Exception
+	 */
 	public Employee(byte number, String name, double salary) throws Exception {
 		setEmpNumber(number);
 	}
 	
 	//List of set Methods for 
+	/**
+	 * 
+	 * @param number
+	 * @throws Exception
+	 */
 	public void setEmpNumber(byte number) throws Exception {
 		
 		if(number > 0 && number <= 50)
@@ -42,12 +67,25 @@ public class Employee {
 			{ throw new Exception("Invalid input!Please enter an employee's salary greater than 0 and less $200,000");}
 	}
 	//List of get Methods for the project 
+	
+	/**
+	 * Gets the employee's number
+	 * @return empNumber - the employee's number
+	 */
 	public byte getEmpNumber() {
 		return empNumber;
 	}
+	/**
+	 * Gets the employee's name
+	 * @return empName - the employee's name
+	 */
 	public String getEmpName() {
 		return empName;
 	}
+	/**
+	 * Gets the employee's salary
+	 * @return empSalary - the employee's salary
+	 */
 	public double getEmpSalary() {
 		return empSalary;
 	}
